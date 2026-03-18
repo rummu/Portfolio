82373dfae24ce447c35aae020f4d0bb9ea09ae56
rummu <rummanahmad05@gmail.com>
2026-03-18 09:23:36 +0530

    Fix FontAwesome production CDN link

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.1/css/all.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <link rel="stylesheet" href="https://cdn.rawgit.com/jpswalsh/academicons/master/css/academicons.min.css" />
         <script
           dangerouslySetInnerHTML={{
