462106650b9981ccdc8915a86753f6ccf1127a4f
rummu <rummanahmad05@gmail.com>
2026-03-18 09:16:19 +0530

    Fix Vercel Next.js 404 Routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      },
-    ];
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:8000/api/:path*",
+        },
+      ];
+    }
+    return [];
   },
 };
 
